0079fdb11932046d7245df5bb7dcdcb405bb675d
AJAY KESALE <139203129+ajaykesale@users.noreply.github.com>
2024-03-09 18:36:31 +0530

    This is a To-Do app List

---
+++ To-Do List/To-do.js
@@ -0,0 +1,24 @@
+let inputs = document.getElementById("inp");
+let text = document.querySelector(".text");
+
+function Add(){
+    if(inputs.value == "")
+    {
+        
+        alert("Please Enter Task...")
+    }else{
+        
+        const newEle = document.createElement("ul");
+        newEle.innerHTML = `${inputs.value} <i class="material-icons">delete</i> `;
+        text.appendChild(newEle);
+        inputs.value = "";
+        newEle.querySelector("i").addEventListener("click",remove);
+        function remove(){
+            newEle.remove();
+            
+            
+        }  
+    }
+}
+
+
